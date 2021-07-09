f38fa804d0f59e68afca72ee6f2cce5bc6772af6
Chen Jian <32921212+JxClur@users.noreply.github.com>
2021-07-10 01:00:32 +0800

    Create tinypic_new.py

---
+++ tinypic_new.py
@@ -0,0 +1,38 @@
+import tinify
+import argparse
+
+"""
+User Input
+"""
+my_parser = argparse.ArgumentParser(description='Compress picture with TinyPNG API')
+my_parser.add_argument('-S', '--source-file', type=str, dest='SOURCE',help="Input the original picture you want to compress.")
+my_parser.add_argument('-O', '--dest-file',type=str, dest='DEST', help="Specify the location you want to save to.")
+args = my_parser.parse_args()
+source_file = args.SOURCE
+dest_file = args.DEST
+
+
+"""
+Main Program
+"""
+def compress_image():
+	try:
+		with open(source_file, 'rb') as source:
+			source_data = source.read()
+			result_data = tinify.from_buffer(source_data).to_buffer()
+			with open(dest_file, 'wb') as dst:
+					dst.write(result_data)
+					dst.close()
+			source.close()
+			
+	except tinify.errors.AccountError:
+		print('Invalid API Key.')
+	except tinify.errors.ConnectionError:
+		print('Please check your internet connection.')
+	except tinify.errors.ClientError:
+		print('File type is not supported.')
+
+if __name__ == "__main__":
+	API_KEY = "<API_KEY>"
+	tinify.key = API_KEY
+	compress_image()
